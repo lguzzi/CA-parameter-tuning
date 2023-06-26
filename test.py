411bd6091064c1b51d1eb257a277e9876e242fb0
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2023-06-26 10:57:37 +0200

    First implementation of test script

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 import subprocess
 
-subprocess.run(['cmsRun','step3_RAW2DIGI_RECO_VALIDATION_DQM.py'])
+CAThetaCutBarrel = 0.003
+subprocess.run(['cmsRun','step3_RAW2DIGI_RECO_VALIDATION_DQM.py', f'CAThetaCutBarrel={CAThetaCutBarrel}'])
