9f47bc77138278d3a54cf6689251d67fb693ee6c
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2023-06-26 11:08:19 +0200

    Print process output
    Test efficiency retrieval  from output
    
    Compute global efficiency
    
    Compute efficiency from validator output

--- test.py
+++ test.py
@@ -1,4 +1,38 @@
 import subprocess
+from io import StringIO
+
 
 CAThetaCutBarrel = 0.003
-subprocess.run(['cmsRun','step3_RAW2DIGI_RECO_VALIDATION_DQM.py', f'CAThetaCutBarrel={CAThetaCutBarrel}'])
+CAThetaCutForward = 0.004
+dcaCutInnerTriplet = 0.16
+dcaCutOuterTriplet = 0.26
+
+#process
+
+output = subprocess.run(['cmsRun','step3_RAW2DIGI_RECO_VALIDATION_DQM.py', 
+                f'CAThetaCutBarrel={CAThetaCutBarrel}',
+                f'CAThetaCutForward={CAThetaCutForward}',
+                f'dcaCutInnerTriplet={dcaCutInnerTriplet}',
+                f'dcaCutOuterTriplet={dcaCutOuterTriplet}'],
+                capture_output=True,
+                text=True)
+
+# ouputFile = open("output.txt", "w")
+# ouputFile.write(output.stderr)
+# ouputFile.close()
+
+outputLines = StringIO(output.stderr)
+totalRec=0
+totalAss=0
+while line:=outputLines.readline():
+    if 'Collection' in line:
+        line = outputLines.readline()
+        line = outputLines.readline()
+        line = outputLines.readline()
+        totalRec += int(line.split()[-1])
+        line = outputLines.readline()
+        totalAss += int(line.split()[-1])
+
+efficiency = totalAss / totalRec
+print(efficiency)
+
