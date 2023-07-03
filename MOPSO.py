61f556b8368459ea4292ecacc344d2ca808a3511
bucket420 <hoadh-23@rhodes.edu>
2023-07-03 15:33:33 +0200

    Refactor code and add default metrics

--- MOPSO.py
+++ MOPSO.py
@@ -3,6 +3,20 @@ import csv
 import subprocess
 import uproot
 
+def get_metrics(uproot_file, id):
+    tree = uproot_file['simpleValidation' + str(id)]['output']
+    total_rec = tree['rt'].array()[0]
+    total_ass = tree['at'].array()[0]
+    total_sim = tree['st'].array()[0]
+    
+    return [total_sim / total_ass, (total_rec - total_ass) / total_rec]
+
+def write_csv(filename, matrix):
+    with open(filename, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        for i in range(len(matrix)):
+            writer.writerow(matrix[i]) 
+
 class Particle:
     def __init__(self, lb=-10, ub=10, num_objectives=2):
         self.position = np.random.uniform(lb, ub)
@@ -20,29 +34,17 @@ class Particle:
 
     def update_position(self, lb, ub):
         self.position = np.clip(self.position + self.velocity, lb, ub)
-
-    # def evaluate_fitness(self, objective_functions):
-    #     self.fitness = np.array([obj_func(self.position) for obj_func in objective_functions])
-    #     if any(self.fitness < self.best_fitness):
-    #         self.best_fitness = self.fitness
-    #         self.best_position = self.position
     
-    def evaluate_fitness(self, filename, id):
-        with uproot.open(filename) as output:
-            out = output['simpleValidation' + str(id)]['output']
-            totalRec = out['rt'].array()[0]
-            totalAss = out['at'].array()[0]
-            totalSim = out['st'].array()[0]
-            
-        self.fitness = np.array([totalSim / (totalAss + 1), (totalRec - totalAss) / (totalRec + 1)])
+    def evaluate_fitness(self, uproot_file, id):
+        self.fitness = np.array(get_metrics(uproot_file, id))
         
         if any(self.fitness < self.best_fitness):
             self.best_fitness = self.fitness
             self.best_position = self.position
 
 class PSO:
-    def __init__(self, objective_functions, lb, ub, num_particles=50, w=0.5, c1=1, c2=1, num_iterations=100, max_iter_no_improv=None, tol=None):
-        self.objective_functions = objective_functions
+    def __init__(self, lb, ub, num_objectives=2, num_particles=50, w=0.5, c1=1, c2=1, 
+                 num_iterations=100, max_iter_no_improv=None, tol=None):
         self.num_particles = num_particles
         self.lb = lb
         self.ub = ub
@@ -52,24 +54,28 @@ class PSO:
         self.num_iterations = num_iterations
         self.max_iter_no_improv = max_iter_no_improv
         self.tol = tol
-
+        self.num_objectives = num_objectives
         self.particles = [Particle(lb, ub) for _ in range(num_particles)]
         self.global_best_position = np.zeros_like(lb)
         self.global_best_fitness = [np.inf, np.inf] #TODO: you can improve it to be a list of size num_objectives
         self.history = []
-        self.write_params('parameters.csv')
+        write_csv('parameters.csv', [self.particles[i].position for i in range(self.num_particles)])
             
-    def write_params(self, filename):
-        with open(filename, 'w', newline='') as csvFile:
-            writer = csv.writer(csvFile)
-            for i in range(self.num_particles):
-                writer.writerow(self.particles[i].position)     
+    # def write_params(self, filename):
+    #     with open(filename, 'w', newline='') as csvFile:
+    #         writer = csv.writer(csvFile)
+    #         for i in range(self.num_particles):
+    #             writer.writerow(self.particles[i].position)     
 
     def optimize(self):
+        uproot_file = None
         for i in range(self.num_iterations):
-            subprocess.run(['cmsRun','reconstruction.py', "outputFileName=" + "output/output" + str(i) + ".root"])
+            write_csv('history/parameters/iteration' + str(i) + '.csv', [self.particles[i].position for i in range(self.num_particles)])
+            validation_result = "history/validation/iteration" + str(i) + ".root"
+            subprocess.run(['cmsRun','reconstruction.py', "outputFileName=" + validation_result])
             for j, particle in enumerate(self.particles):
-                particle.evaluate_fitness("output/output" + str(i) + ".root", j)
+                uproot_file = uproot.open(validation_result)
+                particle.evaluate_fitness(uproot_file, j)
 
                 if all(particle.fitness < self.global_best_fitness): 
                     self.global_best_fitness = particle.fitness
@@ -78,16 +84,18 @@ class PSO:
                 self.update_particle_best(particle)
                 self.update_global_best()
 
-                particle.update_velocity(
-                    self.global_best_position, self.w, self.c1, self.c2)
+                particle.update_velocity(self.global_best_position, self.w, self.c1, self.c2)
                 particle.update_position(self.lb, self.ub)
-
-            self.write_params('parameters.csv')
-            self.write_params('params/parameters' + str(i) + '.csv')
-            self.history.append(self.global_best_fitness)
+                
+            uproot_file.close()
+            write_csv('parameters.csv', [self.particles[i].position for i in range(self.num_particles)])
+            self.history.append(np.concatenate([self.global_best_position, self.global_best_fitness]))
 
         pareto_front = self.get_pareto_front()
-        return pareto_front
+        
+        write_csv('history/history.csv', self.history)
+        write_csv('history/pareto_front.csv', [np.concatenate([pareto_front[i].position, pareto_front[i].fitness]) 
+                                               for i in range(len(pareto_front))])
 
     def update_particle_best(self, particle):
         if any(particle.fitness < particle.best_fitness):
@@ -116,9 +124,8 @@ class PSO:
         return pareto_front
 
     def calculate_crowding_distance(self, pareto_front):
-        num_objectives = len(self.objective_functions)
         crowding_distances = {particle: 0 for particle in pareto_front}
-        for objective_index in range(num_objectives):
+        for objective_index in range(self.num_objectives):
             # Sort the Pareto front by the current objective function
             pareto_front_sorted = sorted(pareto_front, key=lambda x: x.fitness[objective_index])
             crowding_distances[pareto_front_sorted[0]] = np.inf
--- optimize.py
+++ optimize.py
@@ -1,47 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from MOPSO import PSO
-# the optimize function is wrong, the 1D does not work anymore, there is no test case so idk if the code works, 
-
-def objective_function_1(x):
-    return np.sin(x[0]) + np.sin(x[1])
-    # return x**2
-
-
-def objective_function_2(x):
-    return np.cos(x[0])+np.cos(x[1])
-    # return (x-2)**2
-
 
 # define the lower and upper bounds
-lb = [0.0, 0.0, 0.0, 0.0] #!!!
-ub = [0.006, 0.03, 0.2, 1.0] #!!!
+lb = [0.0, 0.0, 0.0, 0.2] #!!!
+ub = [0.006, 0.03, 0.2, 0.8] #!!!
 
 # create the PSO object
-pso = PSO(objective_functions=[objective_function_1, objective_function_2], 
-          lb=lb, ub=ub, num_particles=200, num_iterations=20, w=0.5, 
+pso = PSO(lb=lb, ub=ub, num_particles=200, num_iterations=3, w=0.5, 
           c1=1, c2=1, max_iter_no_improv=None, tol=None)
 
 # run the optimization algorithm
-pareto_front = pso.optimize()
+pso.optimize()
 
-# print the results
-print("Global best position:", pso.global_best_position)
-print("Global best fitness:", pso.global_best_fitness)
-
-# plot the Pareto front
-pareto_x = [particle.fitness[0] for particle in pareto_front]
-print(pareto_x)
-pareto_y = [particle.fitness[1] for particle in pareto_front]
-plt.scatter(pareto_x, pareto_y)
-plt.xlabel("Objective 1")
-plt.ylabel("Objective 2")
-plt.title("Pareto Front")
-plt.show()
-
-# plot the convergence history
-plt.plot(pso.history)
-plt.xlabel("Iteration")
-plt.ylabel("Global Best Fitness")
-plt.title("Convergence History")
-plt.show()
