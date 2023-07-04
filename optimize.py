9afffc2a694e9a35f49eec425ee584fee4fe77ea
bucket420 <hoadh-23@rhodes.edu>
2023-07-04 18:38:09 +0200

    add data from new run

--- optimize.py
+++ optimize.py
@@ -1,8 +1,8 @@
 from MOPSO import PSO
 
 # define the lower and upper bounds
-lb = [0.0, 0.0, 0.0, 0.2] #!!!
-ub = [0.006, 0.03, 0.2, 0.8] #!!!
+lb = [0.0, 0.0, 0.0, 0.0] #!!!
+ub = [0.006, 0.03, 0.2, 1.0] #!!!
 
 # create the PSO object
 pso = PSO(lb=lb, ub=ub, num_particles=200, num_iterations=20, w=0.5, 
