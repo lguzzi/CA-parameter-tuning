dea2217763f40eef1368b0d74f21f9288e02cf1f
Chris Hoang <hoadh-23@rhodes.edu>
2023-07-19 00:02:34 +0200

    add new data

--- optimize.py
+++ optimize.py
@@ -8,7 +8,7 @@ args = parser.parse_args()
 
 # define the lower and upper bounds
 lb = [0.0, 0.0, 0.0, 0.0] #!!!
-ub = [0.006, 0.03, 0.2, 1.0] #!!!
+ub = [0.006, 0.03, 0.3, 1.0] #!!!
 
 # create the PSO object
 pso = PSO(lb=lb, ub=ub, num_particles=200, num_iterations=100, w=0.5, 
