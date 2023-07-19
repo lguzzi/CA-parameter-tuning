b36b66467e6579e1d5b7eb69b78f5cc88a5b9067
bucket420 <hoadh-23@rhodes.edu>
2023-07-19 18:58:49 +0200

    update

--- MOPSO.py
+++ MOPSO.py
@@ -39,7 +39,7 @@ class Particle:
         if self.best_fitness[0] == 0 or self.best_fitness[1] == 0:
             self.fitness = np.ones(self.num_objectives)
             
-        if all(self.fitness < self.best_fitness):
+        if all(self.fitness <= self.best_fitness):
             self.best_fitness = self.fitness
             self.best_position = self.position
 
@@ -47,47 +47,41 @@ class PSO:
     def __init__(self, lb, ub, num_objectives=2, num_particles=50, w=0.5, c1=1, c2=1, 
                  num_iterations=100, continuing=False, max_iter_no_improv=None, tol=None):
         if not continuing:
-            self.lb = lb
-            self.ub = ub
-            self.num_objectives = num_objectives
-            self.num_particles = num_particles
-            self.w = w
-            self.c1 = c1
-            self.c2 = c2
-            self.num_iterations = num_iterations
-            self.max_iter_no_improv = max_iter_no_improv
-            self.tol = tol
-            self.particles = [Particle(lb, ub) for _ in range(num_particles)]
-            self.global_best_position = np.zeros_like(lb)
-            self.global_best_fitness = np.ones(num_objectives)
-            self.iteration = 0
-            write_csv('parameters.csv', [self.particles[i].position for i in range(self.num_particles)])
             saved_params = {
-                "lb": self.lb,
-                "ub": self.ub,
-                "num_objectives": self.num_objectives,
-                "num_particles": self.num_particles,
-                "w": self.w,
-                "c1": self.c1,
-                "c2": self.c2,
-                "max_iter_no_improv": self.max_iter_no_improv,
-                "tol": self.tol,
+                "lb": lb,
+                "ub": ub,
+                "num_objectives": num_objectives,
+                "num_particles": num_particles,
+                "w": w,
+                "c1": c1,
+                "c2": c2,
+                "max_iter_no_improv": max_iter_no_improv,
+                "tol": tol,
             }
             with open('history/pso_saved_params.json', 'w') as f:
                 json.dump(saved_params, f, indent=4)
         else:
             with open('history/pso_saved_params.json') as f:
                 saved_params = json.load(f)
-            self.lb = saved_params["lb"]
-            self.ub = saved_params["ub"]
-            self.num_objectives = saved_params["num_objectives"]
-            self.num_particles = saved_params["num_particles"]
-            self.w = saved_params["w"]
-            self.c1 = saved_params["c1"]
-            self.c2 = saved_params["c2"]
+                
+        self.lb = saved_params["lb"]
+        self.ub = saved_params["ub"]
+        self.num_objectives = saved_params["num_objectives"]
+        self.num_particles = saved_params["num_particles"]
+        self.w = saved_params["w"]
+        self.c1 = saved_params["c1"]
+        self.c2 = saved_params["c2"]
+        self.max_iter_no_improv = saved_params["max_iter_no_improv"]
+        self.tol = saved_params["tol"]
+        
+        if not continuing:
             self.num_iterations = num_iterations
-            self.max_iter_no_improv = max_iter_no_improv
-            self.tol = saved_params["tol"]
+            self.particles = [Particle(lb, ub) for _ in range(num_particles)]
+            self.global_best_position = np.zeros_like(lb)
+            self.global_best_fitness = np.ones(num_objectives)
+            self.iteration = 0
+        else:
+            self.num_iterations = num_iterations
             num_params = len(self.lb)
             global_state = read_csv('history/global_state.csv')[0]
             self.global_best_position = np.array(global_state[:num_params], dtype=float)
@@ -102,30 +96,16 @@ class PSO:
                                        best_position=np.array(individual_states[i][2*num_params:3*num_params], dtype=float),
                                        best_fitness=np.array(individual_states[i][3*num_params:], dtype=float)
                                        ) for i in range(self.num_particles)]
+        write_csv('parameters.csv', [particle.position for particle in self.particles])
             
     def optimize(self):
         uproot_file = None
         if not self.iteration:
             # clear old data, probably not the best way to do this
-            os.system("rm -rf history/parameters/*")
+            os.system("rm -rf history/particles/*")
             os.system("rm -rf history/validation/*")
-            os.system("rm -rf history/pareto_front/*")
-            all_time_pareto_front = None
-        else:
-            # loading all-time pareto front
-            temp = read_csv('history/all_time_pareto_front.csv')
-            all_time_pareto_front = [Particle(lb=self.lb, 
-                                              ub=self.ub, 
-                                              num_objectives=self.num_objectives,
-                                              position=particle[:-self.num_objectives],
-                                              fitness=particle[-self.num_objectives:]
-                                              ) for particle in temp]
             
         for i in range(self.num_iterations):
-            # save tracking parameters
-            write_csv('history/parameters/iteration' + str(self.iteration) + '.csv', 
-                      [self.particles[i].position for i in range(self.num_particles)])
-            
             # run reconstruction and validate tracks
             validation_result = "history/validation/iteration" + str(self.iteration) + ".root"
             subprocess.run(['cmsRun','reconstruction.py', "inputFiles=file:step2.root", 
@@ -135,25 +115,22 @@ class PSO:
             for j, particle in enumerate(self.particles):
                 uproot_file = uproot.open(validation_result)
                 particle.evaluate_fitness(uproot_file, j)
-                if all(particle.fitness < self.global_best_fitness): 
+                if all(particle.fitness <= self.global_best_fitness): 
                     self.global_best_fitness = particle.fitness
                     self.global_best_position = particle.position
                 particle.update_velocity(self.global_best_position, self.w, self.c1, self.c2)
             uproot_file.close()
-                
-            # save current pareto front
-            pareto_front = self.get_pareto_front()
-            write_csv('history/pareto_front/iteration' + str(self.iteration) + '.csv', 
-                      [np.concatenate([pareto_front[i].position, pareto_front[i].fitness]) 
-                       for i in range(len(pareto_front))])  
             
-            # update all-time pareto front
-            all_time_pareto_front = self.update_all_time_pareto_front(all_time_pareto_front, pareto_front)
+            # save position and fitness
+            write_csv('history/particles/iteration' + str(self.iteration) + '.csv', 
+                      [np.concatenate([particle.position, particle.best_fitness]) for particle in self.particles])
 
             # update positions
             for j, particle in enumerate(self.particles):
                 particle.update_position(self.lb, self.ub)
-            write_csv('parameters.csv', [self.particles[i].position for i in range(self.num_particles)])
+                
+            # update input parameters for next iteration
+            write_csv('parameters.csv', [particle.position for particle in self.particles])
             
             # save states of particles
             write_csv('history/individual_states.csv', 
@@ -165,41 +142,22 @@ class PSO:
             write_csv('history/global_state.csv', 
                       [np.concatenate([self.global_best_position, self.global_best_fitness, [self.iteration]])])
         
-        write_csv('history/all_time_pareto_front.csv', 
-            [np.concatenate([all_time_pareto_front[i].position, all_time_pareto_front[i].fitness]) 
-             for i in range(len(all_time_pareto_front))]) 
+        self.get_pareto_front()
 
     def get_pareto_front(self):
-        pareto_front = []
-        for particle in self.particles:
-            dominated = False
-            for other_particle in self.particles:
-                if all(particle.fitness > other_particle.fitness):
-                    dominated = True
-                    break
-            if not dominated:
-                pareto_front.append(particle)
-        # Sort the Pareto front by crowding distance
-        # crowding_distances = self.calculate_crowding_distance(pareto_front)
-        # pareto_front.sort(key=lambda x: crowding_distances[x], reverse=True)
-        return pareto_front
-    
-    def update_all_time_pareto_front(self, all_time_pareto_front, pareto_front):
-        if not all_time_pareto_front:
-            return pareto_front
-        
-        pareto_front += all_time_pareto_front
-        new_all_time_pareto_front = []
-        for particle in pareto_front:
-            dominated = False
-            for other_particle in pareto_front:
-                if all(particle.fitness > other_particle.fitness):
-                    dominated = True
-                    break
-            if not dominated:
-                new_all_time_pareto_front.append(particle)
-        return new_all_time_pareto_front
-        
+            pareto_front = []
+            particles = np.concatenate([read_csv('history/particles/iteration' + str(i) + '.csv') 
+                                    for i in range(self.num_iterations)])
+            for particle in particles:
+                dominated = False
+                for other_particle in particles:
+                    if all(particle[4:] > other_particle[4:]) or (particle[4] == other_particle[4] and particle[5] > other_particle[5]) or (particle[4] > other_particle[4] and particle[5] == other_particle[5]):
+                        dominated = True
+                        break
+                if not dominated:
+                    pareto_front.append(particle)
+            write_csv('history/pareto_front.csv', pareto_front)
+            return np.unique(pareto_front, axis=0)
 
     def calculate_crowding_distance(self, pareto_front):
         crowding_distances = {particle: 0 for particle in pareto_front}
