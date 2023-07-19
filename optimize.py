1a84d57bd631531d1c839eda0bfe3b3d2f897ce2
bucket420 <hoadh-23@rhodes.edu>
2023-07-19 14:37:07 +0200

    add duplicates rate

--- optimize.py
+++ optimize.py
@@ -11,7 +11,7 @@ lb = [0.0, 0.0, 0.0, 0.0] #!!!
 ub = [0.006, 0.03, 0.3, 1.0] #!!!
 
 # create the PSO object
-pso = PSO(lb=lb, ub=ub, num_particles=200, num_iterations=100, w=0.5, 
+pso = PSO(lb=lb, ub=ub, num_particles=200, num_iterations=20, w=0.5, 
           c1=1, c2=1, max_iter_no_improv=None, tol=None, continuing=args.continuing)
 
 # run the optimization algorithm
